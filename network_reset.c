72474cef046161717092a7ff20691f0edfb019cc
binexec <bin4ry@live.com>
2016-08-30 00:38:14 -0700

    User can customize periods using external cfg files
    The periods for normal and alternative checking frequencies, as well as
    power cycle time can be customized by the user in the file
    network_reset.cfg. This file is created automatically if it's not found.
    
    Additionally, all definitions for the program has been moved to
    network_reset.h

--- network_reset.c
+++ network_reset.c
@@ -6,45 +6,101 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 #include <signal.h>
 #include <pigpio.h>
+#include "network_reset.h"
+
+//Default values for user definable variables loaded from external config file
+int net_check_period_std = NET_CHECK_PERIOD_STD;
+int net_check_period_alt = NET_CHECK_PERIOD_ALT;
+int power_cycle_time = POWER_CYCLE_TIME;
 
 
-//Pin declarations
-#define RELAY1_PIN	2		//Relay is active LOW!
-#define GLED_PIN	3		//LED is active HIGH!
-#define RLED_PIN	4		//LED is active HIGH!
-#define BTN_PIN		17		//Button is active LOW!
+void createCfgFile()
+{
+	FILE *cfg = fopen(CFG_FILENAME, "w");
 
-//Times declarations. All in seconds
-#define NET_CHECK_PERIOD_STD	30	//How often to check network connectivitiy under normal circumstances
-#define NET_CHECK_PERIOD_ALT	180	//When a network failure occurs, how often to check until network is back? This value must be greater than the time it takes for your router/modem to reboot.
-#define RELAY_TOGGLE_TIME	10 	//Relay Cycle time
-#define LED_OK_PULSE		0.25	//When ping suceeded, how long to pulse the green LED
+	fprintf(cfg, "%s\n\n", CFG_HEADER);
+	fprintf(cfg, "NET_CHECK_PERIOD_STD %d\n", NET_CHECK_PERIOD_STD);
+	fprintf(cfg, "NET_CHECK_PERIOD_ALT %d\n", NET_CHECK_PERIOD_ALT);
+	fprintf(cfg, "POWER_CYCLE_TIME %d\n", POWER_CYCLE_TIME);
 
-//System command used to ping Google DNS to test internet connectivity
-#define PING_CMD "ping -c 1 8.8.8.8 > /dev/null 2>&1"
+	fclose(cfg);
+}
 
+void parseCfgFile()
+{
+	char buf[CFG_MAX_LINE_CHAR];
+	char *config_str;		//The string represent a variable to set from the line
+	int config_val;			//The value associated with the variable 
+
+	FILE *cfg = fopen(CFG_FILENAME, "r");	
+
+	//Check if the configuration file already exists
+	if(cfg == NULL)
+	{
+		printf("Configuration file \"%s\" does not exist. Creating a new one...\n", CFG_FILENAME);
+		createCfgFile();
+		return;
+	}
+
+	//Stop reading the cfg file if it's empty, or missing the correct header
+	if(fgets(buf, CFG_MAX_LINE_CHAR, cfg) == NULL || strncmp(buf, CFG_HEADER, strlen(CFG_HEADER)) != 0)
+	{
+		printf("Configuration file does not seem to be valid. Using default values instead.\n");
+		fclose(cfg);
+		return;
+	}
+
+	//Parse the settings in the config file line by line
+	while(fgets(buf, CFG_MAX_LINE_CHAR, cfg) != NULL)
+	{
+		//Ignore empty lines
+		if(buf[0] == '\n') 
+			continue;
+
+		//Extract the variable name and associated value from the line
+		config_str = strtok(buf, " \n\t");		
+		config_val = atoi(strtok(NULL, " \n\t"));
+		//printf("PARSED: %s,%d\n", config_str, config_val);
+		
+		//Process the command
+		if(strcmp(config_str, NET_CHECK_PERIOD_STD_STR) == 0)
+			net_check_period_std = config_val;
+
+		else if(strcmp(config_str, NET_CHECK_PERIOD_ALT_STR) == 0)
+			net_check_period_alt = config_val;
+
+		else if(strcmp(config_str, POWER_CYCLE_TIME_STR) == 0)
+			power_cycle_time = config_val;
+
+		else
+			printf("Unrecognized command \"%s\"\n", config_str);
+	}
+
+	fclose(cfg);
+}
 
 void sig_handler(int signo)
 {
 	switch(signo)
 	{
 		case SIGINT:
-			printf("SIGINT CAPTURED. Exiting...\n");
+			printf("\n***SIGINT CAPTURED*** \nExiting...\n");
 			break;
 		case SIGTERM:
-			printf("SIGTERM CAPTURED. Exiting...\n");
+			printf("\n***SIGTERM CAPTURED*** \nExiting...\n");
 			break;
 		case SIGABRT:
-			printf("SIGABRT CAPTURED. Exiting...\n");
+			printf("\n***SIGABRT CAPTURED*** \nExiting...\n");
 			break;
 		default:
-			printf("CAPTURED UNEXPECTED SIGNAL %d. Ignoring...\n", signo);
+			printf("\nCAPTURED UNEXPECTED SIGNAL %d. Ignoring...\n", signo);
 			return;
 	}
 
-	//Set output GPIOs to initial values and exit the program
+	//Set output GPIOs to reset values and exit the program
 	gpioWrite(RELAY1_PIN, 1);	
 	gpioWrite(RLED_PIN, 0);	
 	gpioWrite(GLED_PIN, 0);	
@@ -69,17 +125,21 @@ void printTimestamp()
 void cycleRelay()
 {
 	gpioWrite(RELAY1_PIN, 0);	
-	time_sleep(RELAY_TOGGLE_TIME);
+	time_sleep(power_cycle_time);
 	gpioWrite(RELAY1_PIN, 1);
 }
 
 int main(int argc, char *argv[])
 {
 	double start;
-	int net_check_period = NET_CHECK_PERIOD_STD;
+	int current_checking_period;
 
 	printTimestamp();
 	printf("Program Started!\n");
+	
+	//Parse the external config file if exists
+	parseCfgFile();
+	current_checking_period = net_check_period_std;
  	
 	//Initialize the piggpio library
 	if (gpioInitialise() < 0)
@@ -110,8 +170,6 @@ int main(int argc, char *argv[])
 	//Start timer
 	start = time_time();
 
-	/*End of initialization*/
-
 	//Main control loop
 	for(;;)
 	{
@@ -119,7 +177,7 @@ int main(int argc, char *argv[])
 		if(gpioRead(BTN_PIN) == 0)
 		{
 			printTimestamp();
-			printf("Manual Reset! Power Cycling for %d seconds. Next check in %d seconds\n", RELAY_TOGGLE_TIME, NET_CHECK_PERIOD_ALT);
+			printf("Manual Reset! Power Cycling for %d seconds. Next check in %d seconds\n", POWER_CYCLE_TIME, NET_CHECK_PERIOD_ALT);
 		
 			//Turn on the RED LED, and turn off the GREEN LED (if on)
 			gpioWrite(RLED_PIN, 1);	
@@ -133,19 +191,19 @@ int main(int argc, char *argv[])
 
 			//Reset timer into alt mode
 			start = time_time();
-			net_check_period = NET_CHECK_PERIOD_ALT;
+			current_checking_period = net_check_period_alt;
 			continue;
 		}
 		
 		//Skip network checking if the wait period's not up yet
-		if((time_time() - start) < net_check_period)
+		if((time_time() - start) < current_checking_period)
 			continue;
 
 		//Check if the network is ok by pinging google's DNS server
 		if(system(PING_CMD) == 0)
 		{
 			printTimestamp();
-			printf("Ping ok! Next check in %d seconds\n", NET_CHECK_PERIOD_STD);
+			printf("Ping ok! Next check in %d seconds\n", net_check_period_std);
 
 			//Turn off the RED LED (if on)
 			gpioWrite(RLED_PIN, 0);	
@@ -157,14 +215,14 @@ int main(int argc, char *argv[])
 			
 			//Reset the timer in normal mode
 			start = time_time();
-			net_check_period = NET_CHECK_PERIOD_STD;
+			current_checking_period = net_check_period_std;
 		}
 
 		//Ping failed indicating network failure
 		else
 		{
 			printTimestamp();
-			printf("Ping failed! Next check in %d seconds\n", NET_CHECK_PERIOD_ALT);
+			printf("Ping failed! Next check in %d seconds\n", net_check_period_alt);
 
 			//Turn on the RED LED, and turn off the GREEN LED (if on)
 			gpioWrite(RLED_PIN, 1);	
@@ -175,7 +233,7 @@ int main(int argc, char *argv[])
 
 			//Reset the timer into alt mode
 			start = time_time();
-			net_check_period = NET_CHECK_PERIOD_ALT;
+			current_checking_period = net_check_period_alt;
 		}
 
 	}
---
+++ network_reset.h
@@ -0,0 +1,30 @@
+#ifndef NETWORK_RESET_H
+#define NETWORK_RESET_H
+
+//Pin declarations
+#define RELAY1_PIN			2		//Relay is active LOW!
+#define GLED_PIN			3		//LED is active HIGH!
+#define RLED_PIN			4		//LED is active HIGH!
+#define BTN_PIN				17		//Button is active LOW!
+
+
+//Default period declarations. All in seconds
+#define NET_CHECK_PERIOD_STD		30		//How often to check network connectivitiy under normal circumstances
+#define NET_CHECK_PERIOD_ALT		90		//When a network failure occurs, how often to check until network is back? This value must be greater than the time it takes for your router/modem to reboot.
+#define POWER_CYCLE_TIME		10	 	//When power cycling, how long to toggle the relay for
+#define LED_OK_PULSE			0.25		//When ping suceeded, how long to pulse the green LED
+
+
+//System command used to ping Google DNS to test internet connectivity
+#define PING_CMD			"ping -c 1 8.8.8.8 > /dev/null 2>&1"
+
+
+//Configuration file  related declarations
+#define CFG_FILENAME			"network_reset.cfg"		//Name of config file
+#define CFG_MAX_LINE_CHAR		100				//Maximum number of characters to be read from each line
+#define CFG_HEADER			"[NETWORK_RESET_CFG]"		//First line of the config file
+#define NET_CHECK_PERIOD_STD_STR	"NET_CHECK_PERIOD_STD"
+#define NET_CHECK_PERIOD_ALT_STR	"NET_CHECK_PERIOD_ALT"
+#define POWER_CYCLE_TIME_STR		"POWER_CYCLE_TIME"
+
+#endif
